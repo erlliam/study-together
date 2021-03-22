7494ac7de892f3c493c35c2dd4ccd6d437b188c9
Erlliam Mejia <tommejas@gmail.com>
2021-03-22 00:42:26 -0400

    Add App.js, small changes in other spots

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="starting-page">
+      <h1>Study Together</h1>
+      <button>Create a room</button>
+      <button>Join a room</button>
+    </div>
+  );
+}
+
+export default App;
