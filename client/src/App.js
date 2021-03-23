cf3c0d496aad37241ab24f6c40c4b333c4cb5f12
Erlliam Mejia <tommejas@gmail.com>
2021-03-23 00:01:30 -0400

    Use spaces instead of non breaking spaces

--- client/src/App.js
+++ client/src/App.js
@@ -68,8 +68,10 @@ function JoinRoom() {
   ];
   let roomElements = rooms.map((room) =>
     <Link to={'/room/' + room.location} key={room.location}>
-      <span>{room.passwordProtected ? 'private' : 'public'}&nbsp;</span>
-      <span>{room.name}&nbsp;</span>
+      <span>{room.passwordProtected ? 'private' : 'public'}</span>
+      {' '}
+      <span>{room.name}</span>
+      {' '}
       <span>{room.usersConnected}/{room.userCapacity}</span>
     </Link>
   );
