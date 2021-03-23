e2d1c9239d655c1be657264739f3daa6fc8f273c
Erlliam Mejia <tommejas@gmail.com>
2021-03-23 00:59:11 -0400

    Handle form events

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import {
+  useState
+} from 'react';
+import {
   BrowserRouter as Router,
   Switch,
   Route,
@@ -33,16 +36,48 @@ function StartingPage() {
 }
 
 function CreateRoom() {
+  let [name, setName] = useState('');
+  let [password, setPassword] = useState('');
+  let [capacity, setCapacity] = useState('6');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // todo: POST form data to server
+    console.log(`
+      name: ${name}
+      password: ${password}
+      capacity: ${capacity}
+    `);
+  }
+
   return (
     <div className="create-room">
       <h1>Create a Room</h1>
-      <form>
-        <label for="create-room-name">Room name</label>
-        <input id="create-room-name" />
-        <label for="create-room-password">Password (optional)</label>
-        <input id="create-room-password" />
-        <label for="create-room-size">Room size (16 max)</label>
-        <input id="create-room-size" />
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <label htmlFor="create-room-name">Room name</label>
+        <input
+          id="create-room-name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          required
+        />
+        <label htmlFor="create-room-password">Password (optional)</label>
+        <input
+          id="create-room-password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <label htmlFor="create-room-capacity">Room capacity (16 max)</label>
+        <input
+          id="create-room-capacity"
+          type="number"
+          min="1" max="16"
+          value={capacity}
+          onChange={e => setCapacity(e.target.value)}
+          required
+        />
         <button>Create room</button>
       </form>
     </div>
