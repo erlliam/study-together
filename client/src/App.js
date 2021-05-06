1870078ffd4b40d1454ccada631ffa769196e2be
Erlliam Mejia <tommejas@gmail.com>
2021-05-06 03:01:20 -0400

    Move CreateRoom into it's own file

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,10 @@ import {
   Switch,
   Route,
   Link,
-  useParams,
-  useHistory,
+  useParams
 } from 'react-router-dom';
+
+import CreateRoom from './CreateRoom';
 
 let apiUrl = 'http://localhost:5000/api'
 
@@ -76,87 +77,6 @@ function StartingPage() {
         <Link to="/create-room">Create a room</Link>
         <Link to="/rooms">Join a room</Link>
       </nav>
-    </div>
-  );
-}
-
-function CreateRoom() {
-  // todo: Don't allow names that only contain spaces
-  /* todo: UX
-      remove error message when you start typing
-      display some state that confirms your request is being processed
-      display something in the case of a server error
-  */
-
-  let isMounted = useRef(true);
-  let history = useHistory();
-  let [name, setName] = useState('');
-  let [password, setPassword] = useState('');
-  let [capacity, setCapacity] = useState('4');
-  let [error, setError] = useState();
-
-  useEffect(() => {
-    return (() => {
-      isMounted.current = false;
-    });
-  }, []);
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    let response = await fetch(apiUrl + '/room/create', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: name,
-        password: password,
-        capacity: capacity
-      }),
-      credentials: 'include'
-    });
-    if (isMounted.current) {
-      if (response.ok) {
-        let json = await response.json();
-        history.replace('/room/' + json.id);
-      } else {
-        setError('Invalid input');
-      }
-    }
-  }
-
-  return (
-    <div className="create-room">
-      <h1>Create a Room</h1>
-      {error && <div className="error">{error}</div>}
-      <form autoComplete="off" onSubmit={handleSubmit}>
-        <label htmlFor="create-room-name">Room name</label>
-        <input
-          id="create-room-name"
-          value={name}
-          onChange={e => setName(e.target.value)}
-          autoFocus
-          required
-        />
-        <label htmlFor="create-room-password">Password (optional)</label>
-        <input
-          id="create-room-password"
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <label htmlFor="create-room-capacity">Room capacity (16 max)</label>
-        <input
-          id="create-room-capacity"
-          type="number"
-          min="1" max="16"
-          value={capacity}
-          onChange={e => setCapacity(e.target.value)}
-          required
-        />
-        <button>Create room</button>
-      </form>
     </div>
   );
 }
---
+++ client/src/CreateRoom.js
@@ -0,0 +1,94 @@
+import {
+  useState,
+  useEffect,
+  useRef,
+} from 'react';
+
+import {
+  useHistory,
+} from 'react-router-dom';
+
+let apiUrl = 'http://localhost:5000/api'
+
+function CreateRoom() {
+  // todo: Don't allow names that only contain spaces
+  /* todo: UX
+      remove error message when you start typing
+      display some state that confirms your request is being processed
+      display something in the case of a server error
+  */
+
+  let isMounted = useRef(true);
+  let history = useHistory();
+  let [name, setName] = useState('');
+  let [password, setPassword] = useState('');
+  let [capacity, setCapacity] = useState('4');
+  let [error, setError] = useState();
+
+  useEffect(() => {
+    return (() => {
+      isMounted.current = false;
+    });
+  }, []);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    let response = await fetch(apiUrl + '/room/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        password: password,
+        capacity: capacity
+      }),
+      credentials: 'include'
+    });
+    if (isMounted.current) {
+      if (response.ok) {
+        let json = await response.json();
+        history.replace('/room/' + json.id);
+      } else {
+        setError('Invalid input');
+      }
+    }
+  }
+
+  return (
+    <div className="create-room">
+      <h1>Create a Room</h1>
+      {error && <div className="error">{error}</div>}
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <label htmlFor="create-room-name">Room name</label>
+        <input
+          id="create-room-name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          autoFocus
+          required
+        />
+        <label htmlFor="create-room-password">Password (optional)</label>
+        <input
+          id="create-room-password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <label htmlFor="create-room-capacity">Room capacity (16 max)</label>
+        <input
+          id="create-room-capacity"
+          type="number"
+          min="1" max="16"
+          value={capacity}
+          onChange={e => setCapacity(e.target.value)}
+          required
+        />
+        <button>Create room</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateRoom;
